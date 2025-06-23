027c38174d7a7014d2210f74906cd35e4ff8e1bf
sushantkumaryadav912 <sushantkumaryadav912@gmail.com>
2025-06-23 21:12:26 +0530

    tesitng

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,4 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase client environment variables");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
